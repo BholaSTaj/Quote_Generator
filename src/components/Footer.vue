28551f407c005a5b0dcec9c2a66b840aa60d51b7
BholaSTaj <bholataj2468@gmail.com>
2025-12-05 23:41:26 +0545

    Vue Files

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import {ref} from 'vue'
+    const year = ref("2025")
+</script>
+
+<template>
+    <footer>
+        Created by <span>BST</span> &copy; 2025
+    </footer>
+</template>
+
+<style scoped>
+    footer {
+        position: fixed;
+        margin: 0 auto;
+        bottom: 0;
+        text-align: center;
+        color: #CCD6D9;
+        padding-bottom: 20px;
+    }
+    span{
+        font-family: cursive;
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+    <header>
+        <h1>Quote Generator</h1>
+    </header>
+</template>
+
+<style scoped>
+    header h1{
+        color: #CCD6D9;
+        margin-bottom: 20px;
+    }
+</style>
